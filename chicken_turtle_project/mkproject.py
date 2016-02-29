2d65d58fd3bb93c34e2555813da2466485ab8894
timdiels <timdiels.m@gmail.com>
2016-02-29 18:12:32 +0100

    Add common.graceful_main

--- chicken_turtle_project/mkproject.py
+++ chicken_turtle_project/mkproject.py
@@ -1,11 +1,10 @@
-from chicken_turtle_project.common import configure_logging, get_project
-from chicken_turtle_util.exceptions import UserException, log_exception
+from chicken_turtle_project.common import get_project, graceful_main
+from chicken_turtle_util.exceptions import UserException
 from pathlib import Path
-import sys
-import logging
-import click
 from glob import glob
+import click
 
+import logging
 logger = logging.getLogger(__name__)
     
 def main(): #TODO click to show help message and version; also on mksetup and other tools. Also include the output from -h in the readme automatically, i.e. compile the readme (or maybe reST can? or maybe we should use Sphinx instead?).
@@ -31,94 +30,86 @@ def main(): #TODO click to show help message and version; also on mksetup and ot
     
     py.test will be configured to run test in $project_name.test and subpackages
     '''
-    try:
-        project_root = Path.cwd()
-        configure_logging()
+    graceful_main(_main, logger)
+    
+def _main():
+    project_root = Path.cwd()
+    
+    # Create project if missing
+    project_path = project_root / 'project.py'
+    if not project_path.exists():
+        logger.info('project.py not found, will create from template')
+        project_name = click.prompt('Please pick a name for your project')
+        assert project_name and project_name.strip()
+        with project_path.open('w') as f:
+            f.write(project_template.format(project_name))
+    
+    # Load project info
+    project = get_project()
+    project_name = project['name']
+    pkg_root = project_root / project_name
+    
+    # Create package dir if missing
+    if not pkg_root.exists():
+        pkg_root.mkdir()
+    
+    # Ensure package root __init__.py exists
+    pkg_root_init = pkg_root / '__init__.py'
+    if not pkg_root_init.exists():
+        pkg_root_init.touch()
+    
+    # Ensure package root __init__.py imports __version__    
+    import_line = 'from {}.version import __version__'.format(project_name)
+    with pkg_root_init.open('r') as f:
+        contents = f.read()
+    if import_line not in map(str.strip, contents.splitlines()): 
+        with pkg_root_init.open('w') as f:
+            f.write(import_line + "\n" + contents)
         
-        # Create project if missing
-        project_path = project_root / 'project.py'
-        if not project_path.exists():
-            logger.info('project.py not found, will create from template')
-            project_name = click.prompt('Please pick a name for your project')
-            assert project_name and project_name.strip()
-            with project_path.open('w') as f:
-                f.write(project_template.format(project_name))
+    # Create version.py if missing    
+    version_path = pkg_root / 'version.py'
+    if not version_path.exists():
+        with version_path.open('w') as f:
+            f.write(version_template)
+    
+    # Create test package if missing
+    test_root = pkg_root / 'test'
+    if not test_root.exists():
+        test_root.mkdir()
+    
+    test_root_init = test_root / '__init__.py'
+    if not test_root_init.exists():
+        test_root_init.touch()
         
-        # Load project info
-        project = get_project()
-        project_name = project['name']
-        pkg_root = project_root / project_name
+    # Create requirements.in if missing
+    requirements_in_path = project_root / 'requirements.in'
+    if not requirements_in_path.exists():
+        requirements_in_path.touch()
         
-        # Create package dir if missing
-        if not pkg_root.exists():
-            pkg_root.mkdir()
+    # Create setup.cfg if missing
+    setup_cfg_path = project_root / 'setup.cfg'
+    if not setup_cfg_path.exists():
+        with setup_cfg_path.open('w') as f:
+            f.write(setup_cfg_template)
         
-        # Ensure package root __init__.py exists
-        pkg_root_init = pkg_root / '__init__.py'
-        if not pkg_root_init.exists():
-            pkg_root_init.touch()
-        
-        # Ensure package root __init__.py imports __version__    
-        import_line = 'from {}.version import __version__'.format(project_name)
-        with pkg_root_init.open('r') as f:
-            contents = f.read()
-        if import_line not in map(str.strip, contents.splitlines()): 
-            with pkg_root_init.open('w') as f:
-                f.write(import_line + "\n" + contents)
-            
-        # Create version.py if missing    
-        version_path = pkg_root / 'version.py'
-        if not version_path.exists():
-            with version_path.open('w') as f:
-                f.write(version_template)
-        
-        # Create test package if missing
-        test_root = pkg_root / 'test'
-        if not test_root.exists():
-            test_root.mkdir()
-        
-        test_root_init = test_root / '__init__.py'
-        if not test_root_init.exists():
-            test_root_init.touch()
-            
-        # Create requirements.in if missing
-        requirements_in_path = project_root / 'requirements.in'
-        if not requirements_in_path.exists():
-            requirements_in_path.touch()
-            
-        # Create setup.cfg if missing
-        setup_cfg_path = project_root / 'setup.cfg'
-        if not setup_cfg_path.exists():
-            with setup_cfg_path.open('w') as f:
-                f.write(setup_cfg_template)
-            
-        # Create .gitignore if missing
-        gitignore_path = project_root / '.gitignore'
-        if not gitignore_path.exists():
-            gitignore_path.touch()
-        
-        # Ensure the right patterns are present in .gitignore
+    # Create .gitignore if missing
+    gitignore_path = project_root / '.gitignore'
+    if not gitignore_path.exists():
+        gitignore_path.touch()
+    
+    # Ensure the right patterns are present in .gitignore
+    with gitignore_path.open('r') as f:
+        content = f.read()
+    patterns = set(map(str.strip, content.readlines()))
+    missing_patterns = gitignore_patterns - patterns
+    if missing_patterns:
         with gitignore_path.open('r') as f:
-            content = f.read()
-        patterns = set(map(str.strip, content.readlines()))
-        missing_patterns = gitignore_patterns - patterns
-        if missing_patterns:
-            with gitignore_path.open('r') as f:
-                f.write('\n'.join(*missing_patterns, content))
-        
-        # Emit warnings for missing files
-        for file in 'LICENSE.txt README.*':
-            if not glob(file):
-                logger.warning("Missing file: {}".format(file))
-        
-        #TODO need be able to enter interpreter for current setup.py derived venv, preferably also first load interpreter.py     
-        
-    except UserException as ex:
-        logger.error(ex.message)
-        sys.exit(1)
-    except Exception as ex:
-        log_exception(logger, ex)
-        sys.exit(2)
+            f.write('\n'.join(*missing_patterns, content))
+    
+    # Emit warnings for missing files
+    for file in 'LICENSE.txt README.*':
+        if not glob(file):
+            logger.warning("Missing file: {}".format(file))     
         
 project_template = """
 project = dict(
@@ -188,20 +179,20 @@ project = dict(
         ],
     },
 )
-"""
+""".lstrip()
 
 version_template = """
 # Versions should comply with PEP440.
 # https://www.python.org/dev/peps/pep-0440/
 # https://python-packaging-user-guide.readthedocs.org/en/latest/distributing/#semantic-versioning-preferred
 __version__ = '1.0.0.dev1'
-"""
+""".lstrip()
 
 setup_cfg_template = """
 [pytest]
 addopts = --basetemp=last_test_runs
 testpaths={}/test
-"""
+""".lstrip()
 
 gitignore_patterns = r'''
 *.orig
